5933baf588c488c0026b9cd6d764ade5cf2b84e4
SHUBHRANIL ROY <shubhranil96@gmail.com>
2026-09-20 12:24:06 +0530

    Add manual column type casting and schema printing
    Added manual casting of columns to integer type and printing schema.

--- transformation basic_.py
+++ transformation basic_.py
@@ -8,3 +8,10 @@ df_filter = df.filter((col("id")>5) & (col("age")>35))
 df_filter.display()
 #display in old sql table format
 df_filter.show()
+
+#casting of columns into proper type manually
+df.withColumn("salary",col("salary").cast("int"))
+df.withColumn("age",col("age").cast("int"))
+df.withColumn("id",col("id").cast("int"))
+#printing the types of the schema
+df.printSchema()
